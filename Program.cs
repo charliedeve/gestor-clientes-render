67fb4600c647d4e0454ff30aab47b187fd1ef5ad
Carlos Garcia <cratosmanolo88@gmail.com>
2025-07-10 10:32:54 -0600

    Migrations

--- Program.cs
+++ Program.cs
@@ -31,4 +31,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+
 app.Run();
